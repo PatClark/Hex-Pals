5530994e60dae47d8d56e5dfb84f2ab5857172ac
PatClark <patrick_w_clark@hotmail.com>
2014-03-24 17:51:13 -0400

    includes special_atk
    and also move_count_down, enemy_cleanup, and updated whoIsGuy which sets inactiveGuy now

--- HexGuy.js
+++ HexGuy.js
@@ -17,6 +17,8 @@ function guy(aSize,anX,aY) {
 	var result = field.findCell(anX,aY);
 
 	thatOneGuy[0].grid=[result.gridX,result.gridY];
+	thatOneGuy[0].spec_atk = [];
+	thatOneGuy[0].color = "red";
 	result.has_guy = 1;
 }
 
@@ -31,18 +33,18 @@ function guy2(aSize,anX,aY) {
 	var result = field.findCell(anX,aY);
 
 	thatOneGuy[1].grid=[result.gridX,result.gridY];
+	thatOneGuy[1].spec_atk = [];
+	thatOneGuy[1].color = "purple";
 	result.has_guy = 2;
 }
 
 function moveGuy(e) {
 	var x = e.pageX-5,
 		y = e.pageY-5,
-		index = activeGuy-1,
+		a = activeGuy-1,
 		msg;
 
-	var pc = thatOneGuy[index];
-	if (index == 0) {var color = "red";}
-	if (index == 1) {var color = "purple";}
+	var pc = thatOneGuy[a];
 
 	var result = field.findCell(x,y);
 	if (result != null && !result.obs) {
@@ -50,41 +52,115 @@ function moveGuy(e) {
 			var newX = result.getX() + result.getA(),
 				newY = result.getY();
 
-			byeOldGuy(index);
+			byeOldGuy(a);
 
 			pc.setX(newX),
 			pc.setY(newY);
 
-			cxt.fillStyle = color;
+			cxt.fillStyle = pc.color;
 			pc.draw(cxt);
 
 			pc.grid=[result.gridX,result.gridY];
-			result.has_guy = index+1;
-			move_count--;
-			//console.log("move_count = " + move_count);
-			msg = "Moves until enemy turn: " + move_count;
-			write_report(c,msg);
+			result.has_guy = a+1;
 
-			var num_foes = thatBadGuy.length;
-			var dead = -1;
-			for (i=0;i<num_foes;i++) {
-				if (pc.grid[0] == thatBadGuy[i].grid[0] && pc.grid[1] == thatBadGuy[i].grid[1]) {
-					dead = i;
-				}
-			}
-			if (dead > -1) {
-				thatBadGuy.splice(dead,1);
-				result.has_foe = false;
-			}
+			move_count_down();
+
+			enemy_cleanup();
+
 			if (move_count <= 0) {
 				foeMove();
 				move_count = 2;
 			}
-		} else if (result.has_guy != index+1) {
+		} else if (result.has_guy != a+1) {
 			activeGuy = result.has_guy;
 			whoIsGuy();
 		} else {
 
+		}
+	}
+}
+
+function move_count_down(aNum) {
+	if (aNum == null) {
+		var num = 1;
+	} else {
+		var num = aNum;
+	}
+	move_count = move_count-num;
+	var msg = "Moves until enemy turn: " + move_count;
+	write_report(c,msg);
+}
+
+function enemy_cleanup() {
+	var a = activeGuy-1,
+		n = inactiveGuy-1,
+		num_foes = thatBadGuy.length,
+		dead = -1,
+		bX,
+		bY;
+	for (var i=0;i<num_foes;i++) {
+		if (thatOneGuy[a].grid[0] == thatBadGuy[i].grid[0] && thatOneGuy[a].grid[1] == thatBadGuy[i].grid[1]) {
+			dead = i;
+		}
+	}
+	if (dead > -1) {
+		bX = thatBadGuy[dead].grid[0];
+		bY = thatBadGuy[dead].grid[1];
+		thatBadGuy.splice(dead,1);
+		grid[bX][bY].has_foe = 0;
+	}
+	num_foes = thatBadGuy.length;
+	dead = -1;
+	for (var i=0;i<num_foes;i++) {
+		if (thatOneGuy[n].grid[0] == thatBadGuy[i].grid[0] && thatOneGuy[n].grid[1] == thatBadGuy[i].grid[1]) {
+			dead = i;
+		}
+	}
+	if (dead > -1) {
+		bX = thatBadGuy[dead].grid[0];
+		bY = thatBadGuy[dead].grid[1];
+		thatBadGuy.splice(dead,1);
+		grid[bX][bY].has_foe = 0;
+	}	
+}
+
+function special_atk(e) {
+	var x = e.pageX-5,
+		y = e.pageY-5,
+		a = activeGuy-1,
+		n = inactiveGuy-1,
+		newX,
+		newY,
+		msg;
+	var pc = thatOneGuy[a].spec_atk;
+	var result = field.findCell(x,y);
+	if (result != null && !result.obs) {
+		for (var i=0;i<pc.length;i++) {
+			if (pc[i].gridX == result.gridX && pc[i].gridY == result.gridY) {
+				console.log("SPECIAL ATTACK ACTIVATED!");
+
+				// moves inactive guy
+				newX = result.getX() + result.getA(),
+				newY = result.getY();
+				byeOldGuy(n);
+				thatOneGuy[n].setX(newX);
+				thatOneGuy[n].setY(newY);
+
+				if (n == 0) {var color = "red";}
+				if (n == 1) {var color = "purple";}
+
+				cxt.fillStyle = color;
+				thatOneGuy[n].draw(cxt);
+
+				thatOneGuy[n].grid=[result.gridX,result.gridY];
+				result.has_guy = n+1;
+				move_count_down();
+				enemy_cleanup();
+
+				thatOneGuy[a].spec_atk = [];
+				redraw();
+				redrawGuys();
+			}
 		}
 	}
 }
@@ -103,6 +179,11 @@ function byeOldGuy(index) {
 }
 
 function whoIsGuy() {
-	if (activeGuy == 1) {var color = "red"} else {if (activeGuy == 2) {var color = "purple";}}
+	if (activeGuy == 1) {
+		inactiveGuy = 2;
+	} else if (activeGuy == 2) {
+			inactiveGuy = 1;
+		}
+	var color = thatOneGuy[activeGuy-1].color
 	console.log("Active Guy is " + color);
 }
